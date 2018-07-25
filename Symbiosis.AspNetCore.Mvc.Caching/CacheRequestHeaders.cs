ed2b68730e2ccd1094c334ea781cd6dbef170ca1
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-07-25 16:21:24 +0600

    Added enum for If-Match and If-None-Match Headers

---
+++ Symbiosis.AspNetCore.Mvc.Caching/CacheRequestHeaders.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+	public enum CacheRequestHeaders
+	{
+		IfNoneMatch,
+		IfMatch
+	}
+}
